dd73bfc0e8f6144a374e489679313929bb4e23c1
HansTheRisk <kloss@protonmail.com>
2020-08-25 10:49:57 +0200

    Second commit

--- src/main/java/main/AppConfig.java
+++ src/main/java/main/AppConfig.java
@@ -1,17 +1,32 @@
 package main;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableConfigurationProperties(AmazonProperties.class)
 public class AppConfig {
+
+    @Autowired
+    private AmazonProperties amazonProperties;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.create();
+        return DynamoDbClient.builder()
+                             .credentialsProvider(DefaultCredentialsProvider.create())
+                             .endpointOverride(URI.create(amazonProperties.getDynamoUrl()))
+                             .region(Region.of(amazonProperties.getRegion()))
+                             .build();
     }
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,20 @@
 package main;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(DynamoDbClient client) {
+        return args -> client.listTables();
     }
 }
